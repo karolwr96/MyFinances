4eaf573144b39a94f79dbf785fb0dde5555fcb7a
Karol Wrębiak <karol7525@gmail.com>
2024-02-06 19:28:20 +0100

    Before logging menu

--- register.php
+++ register.php
@@ -5,13 +5,13 @@ if (isset($_POST['email'])) {
   $okValidation = true;
 
   $userName = $_POST['userName'];
-  if (strlen($userName) < 3 || strlen($userName) > 20) {
+  if (strlen($userName) < 2 || strlen($userName) > 20) {
     $okValidation = false;
-    $_SESSION['e_userName'] = "User name must contain between 3 and 20 characters.";
+    $_SESSION['e_userName'] = "Nname must contain between 2 and 20 characters.";
   }
   if (!ctype_alnum($userName)) {
     $okValidation = false;
-    $_SESSION['e_userName'] = "User name can only consist of letters and numbers.";
+    $_SESSION['e_userName'] = "Name can only consist of letters and numbers.";
   }
 
   $email = $_POST['email'];
@@ -56,7 +56,7 @@ if (isset($_POST['email'])) {
       if ($okValidation) {
         if ($connect->query("INSERT INTO users VALUES (NULL, '$userName', '$hashPassword', '$email')")) {
           $_SESSION['successfulRegistration'] = true;
-          $_SESSION['successfulMessage'] = "Registration successful.";
+          // $_SESSION['successfulMessage'] = "Registration successful. Click to log";
         } else {
           throw new Exception(mysqli_connect_errno());
         }
@@ -66,6 +66,35 @@ if (isset($_POST['email'])) {
   } catch (Exception $error) {
     echo 'Server error';
   }
+}
+
+if (isset($_SESSION['formUsername'])) {
+  unset($_SESSION['formUsername']);
+}
+if (isset($_SESSION['formEmail'])) {
+  unset($_SESSION['formEmail']);
+}
+if (isset($_SESSION['formPassword1'])) {
+  unset($_SESSION['formPassword1']);
+}
+if (isset($_SESSION['formPassword2'])) {
+  unset($_SESSION['formPassword2']);
+}
+if (isset($_SESSION['formRegulations'])) {
+  unset($_SESSION['formRegulations']);
+}
+
+if (isset($_SESSION['e_userName'])) {
+  unset($_SESSION['e_userName']);
+}
+if (isset($_SESSION['e_email'])) {
+  unset($_SESSION['e_email']);
+}
+if (isset($_SESSION['e_password'])) {
+  unset($_SESSION['e_password']);
+}
+if (isset($_SESSION['e_checkbox'])) {
+  unset($_SESSION['e_checkbox']);
 }
 ?>
 
@@ -108,7 +137,7 @@ if (isset($_POST['email'])) {
                                                   ?>" name="userName" id="form3Example1" class="form-control" />
                         <label class="form-label" for="form3Example1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                             <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                          </svg> User name</label>
+                          </svg> Your name</label>
                       </div>
                     </div>
                     <?php
@@ -124,7 +153,7 @@ if (isset($_POST['email'])) {
                                                     echo $_SESSION['formEmail'];
                                                     unset($_SESSION['formEmail']);
                                                   }
-                                                  ?>"name="email" id="form3Example3" class="form-control" />
+                                                  ?>" name="email" id="form3Example3" class="form-control" />
                       <label class="form-label" for="form3Example3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
                           <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
                         </svg> Email address</label>
@@ -138,11 +167,11 @@ if (isset($_POST['email'])) {
 
                     <div class="form-outline mb-2">
                       <input type="password" value="<?php
-                                                  if (isset($_SESSION['formPassword1'])) {
-                                                    echo $_SESSION['formPassword1'];
-                                                    unset($_SESSION['formPassword1']);
-                                                  }
-                                                  ?>"name="password1" id="form3Example4" class="form-control" />
+                                                    if (isset($_SESSION['formPassword1'])) {
+                                                      echo $_SESSION['formPassword1'];
+                                                      unset($_SESSION['formPassword1']);
+                                                    }
+                                                    ?>" name="password1" id="form3Example4" class="form-control" />
                       <label class="form-label" for="form3Example4"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                           <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
                         </svg> Password</label>
@@ -156,11 +185,11 @@ if (isset($_POST['email'])) {
 
                     <div class="form-outline mb-2">
                       <input type="password" value="<?php
-                                                  if (isset($_SESSION['formPassword2'])) {
-                                                    echo $_SESSION['formPassword2'];
-                                                    unset($_SESSION['formPassword2']);
-                                                  }
-                                                  ?>"name="password2" id="form3Example4" class="form-control" />
+                                                    if (isset($_SESSION['formPassword2'])) {
+                                                      echo $_SESSION['formPassword2'];
+                                                      unset($_SESSION['formPassword2']);
+                                                    }
+                                                    ?>" name="password2" id="form3Example4" class="form-control" />
                       <label class="form-label" for="form3Example4"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                           <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
                         </svg> Repeat password</label>
@@ -168,11 +197,10 @@ if (isset($_POST['email'])) {
 
                     <div class="form-check d-flex justify-content-center mb-4">
                       <input class="form-check-input me-2" <?php
-                                    if (isset($_SESSION['formRegulations']))
-                                    {
-                                      echo "checked";
-                                      unset($_SESSION['formRegulations']);
-                                    }?> type="checkbox" value ="" name="regulations" id="form2Example33" />
+                                                            if (isset($_SESSION['formRegulations'])) {
+                                                              echo "checked";
+                                                              unset($_SESSION['formRegulations']);
+                                                            } ?> type="checkbox" value="" name="regulations" id="form2Example33" />
                       <label class="form-check-label" for="form2Example33">
                         I accept the terms
                       </label>
@@ -191,9 +219,14 @@ if (isset($_POST['email'])) {
                         </svg> Register</button>
                     </div>
                     <?php
-                    if (isset($_SESSION['successfulMessage'])) {
-                      echo '<div class="success">' . $_SESSION['successfulMessage'] . '</div>';
-                      unset($_SESSION['successfulMessage']);
+                    if (isset($_SESSION['successfulRegistration'])) {
+                      //echo '<div class="text-center success">' . $_SESSION['successfulMessage'] . '</div>';
+                      echo '<a href="index.html">
+                      <div class="text-center success">
+                      Registration successful. Click to log in.
+                      </div>
+                      </a>';
+                      unset($_SESSION['successfulRegistration']);
                     }
                     ?>
 
